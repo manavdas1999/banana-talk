ab82a6c5b0b7295818491b1a4b406ff211444112
manavdas1999 <dascorp009@gmail.com>
2023-02-08 08:30:13 +0530

    feat: API integration

--- scripts.js
+++ scripts.js
@@ -1,13 +1,35 @@
 
 let btn = document.querySelector("#btn");
-let textComponent = document.querySelector("#text")
+let inputTextComponent = document.querySelector("#text");
+let outputTextComponent = document.querySelector("#output");
+
+let url = "https://api.funtranslations.com/translate/minion.json"
+// let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+// to generate request url
+function getTranslationUrl(text){
+    return url+"?text="+text;
+}
+
+// error handle callback function
+function errorHandler(error){
+    console.log("Error:", error);
+    alert("Some Thing went wrong with server. Please try again later");
+}
+
+// button event click
+function onBtnClick() {
+    var userText = inputTextComponent.value;
+    // console.log(getTranslationUrl(userText))
+    fetch(getTranslationUrl(userText))
+    .then(response => response.json())
+    .then(data => {
+        outputTextComponent.innerText = data.contents.translated;
+    })
+    // .catch(errorHandler);
+
+    // outputComponent.innerText = userText;
+}
 
 btn.addEventListener("click", onBtnClick);
 
-function onBtnClick() {
-    // var name = prompt("Enter your name");
-    // alert("Hello "+name);
-
-    var userText = textComponent.value;
-    console.log(userText);
-}
